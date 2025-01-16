dc0d66e00ae6606121996f4bb7c74206fc3ffac0
Shivang Mishra <githubshiv@gmail.com>
2025-01-16 14:20:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/JHYHDYHDUDJDJNEUBDUBTHEIOMWYI890/riyaXmusicbot",
+    "https://github.com/ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd/riyaXmusicbot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
