8de2e0b54a92da7297a27350a20c63c83e81fc00
Shivang Mishra <githubshiv@gmail.com>
2025-01-16 15:22:38 +0530

    Update start.py

--- PROFESSOR/plugins/bot/start.py
+++ PROFESSOR/plugins/bot/start.py
@@ -25,24 +25,6 @@ from PROFESSOR.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
-
-
-NEXI_VID = [
-"https://telegra.ph/file/6ae3a399b96f70b6fda79.mp4",
-"https://telegra.ph/file/5df37a776933bb427b528.mp4",
-"https://telegra.ph/file/85a35e5a79525b70f5904.mp4",
-"https://telegra.ph/file/75764b093a76d08f51d2c.mp4",
-"https://telegra.ph/file/ea951700bb21f53df70c9.mp4",
-"https://telegra.ph/file/b74553a355a110d9a016b.mp4",
-"https://telegra.ph/file/959dc8b67413e50f1c4a5.mp4",
-"https://graph.org/file/2a7f857f31b32766ac6fc.mp4",
-"https://graph.org/file/83ebf52e8bbf138620de7.mp4",
-"https://graph.org/file/ba7699c28dab379b518ca.mp4",
-
-]
-
-
-
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
